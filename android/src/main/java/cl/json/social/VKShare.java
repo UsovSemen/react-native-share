8b1f30c7463d6c90627f001331ce08a3c3c73eef
Sam <usov.semen@gmail.com>
2018-03-12 17:13:40 +0300

    add VkShare class

---
+++ android/src/main/java/cl/json/social/VKShare.java
@@ -0,0 +1,39 @@
+package cl.json.social;
+
+import android.content.ActivityNotFoundException;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableMap;
+
+/**
+ * Created by disenodosbbcl on 23-07-16.
+ */
+public class VKShare extends SingleShareIntent {
+
+    private static final String PACKAGE = "com.twitter.android";
+    private static final String DEFAULT_WEB_LINK = "https://vk.com/share.php?title={message}&url={url}";
+
+    public TwitterShare(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    @Override
+    public void open(ReadableMap options) throws ActivityNotFoundException {
+        super.open(options);
+        //  extra params here
+        this.openIntentChooser();
+    }
+    @Override
+    protected String getPackage() {
+        return PACKAGE;
+    }
+
+    @Override
+    protected String getDefaultWebLink() {
+        return DEFAULT_WEB_LINK;
+    }
+
+    @Override
+    protected String getPlayStoreLink() {
+        return null;
+    }
+}
